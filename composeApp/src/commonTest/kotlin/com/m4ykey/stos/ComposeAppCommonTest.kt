f2984bebc45c2b610d9295258ce36f18076f8338
m4ykey <michalfr1601@gmail.com>
2025-10-06 21:45:08 +0200

    first commit

---
+++ composeApp/src/commonTest/kotlin/com/m4ykey/stos/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.m4ykey.stos
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
